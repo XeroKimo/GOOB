31e29dfeb6a12f5d886d524fb35bf84115f96aa8
unbent <daniel.polan@hotmail.com>
2019-04-11 04:19:11 -0400

    Added Sounds/UI stuff
    Sounds for,
    -walking
    -jumping
    -charging jump
    -super jump
    -shotgun
    
    *UI is still in progress, please forgive the clutter*

--- Plugins/GameFrwkSessionsPlugin/Intermediate/Build/Win32/UE4/Development/GameFrwkSessionsPlugin/Definitions.GameFrwkSessionsPlugin.h
+++ Plugins/GameFrwkSessionsPlugin/Intermediate/Build/Win32/UE4/Development/GameFrwkSessionsPlugin/Definitions.GameFrwkSessionsPlugin.h
@@ -41,7 +41,7 @@
 #define PROJECT_PRODUCT_IDENTIFIER Terminus_22XX
 #define NDEBUG 1
 #define UE_BUILD_DEVELOPMENT 1
-#define UE_ENGINE_DIRECTORY "D:/Program Files/Epic Games/UE_4.20/Engine/"
+#define UE_ENGINE_DIRECTORY "D:/Program Files (x86)/UE_4.20/Engine/"
 #define ORIGINAL_FILE_NAME "Terminus_22XX.exe"
 #define UE_IS_ENGINE_MODULE 0
 #define UE_BUILD_DEVELOPMENT_WITH_DEBUGGAME 0
