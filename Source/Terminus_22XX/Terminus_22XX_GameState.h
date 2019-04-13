1259f380455bb12196cb883572b55251c05a6f7a
Daniel Polan <farnazman@hotmail.com>
2019-04-12 20:29:25 -0400

    Updated BossTimer
    BossTimer now 30 instead of 60

--- Source/Terminus_22XX/Terminus_22XX_GameState.h
+++ Source/Terminus_22XX/Terminus_22XX_GameState.h
@@ -31,5 +31,5 @@ public:
 	UPROPERTY(BlueprintReadOnly, Replicated)
 		bool StartBossCountDown = false;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Replicated)
-		float BossCountdown = 60.0f;
+		float BossCountdown = 30.0f;
 };
